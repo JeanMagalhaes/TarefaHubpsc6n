a3444739b2d8f52616d13a8649ea9916efd39ff5
Jean Magalhães <jeanmagalhaesdasilva@gmail.com>
2025-05-23 21:27:48 -0300

    feat: model projeto

---
+++ src/main/java/models/Projeto.java
@@ -0,0 +1,29 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
+@Entity
+@Table(name = "projeto")
+
+public class Projeto {
+
+    @Id
+    private int id;
+    private String descricao;
+    private LocalDate dataInicio;
+    private LocalDate dataPrevistaFim;
+
+    public Projeto(int id, String descricao, LocalDate dataInicio, LocalDate dataPrevistaFim) {
+        this.id = id;
+        this.descricao = descricao;
+        this.dataInicio = dataInicio;
+        this.dataPrevistaFim = dataPrevistaFim;
+    }
+
+
+}
